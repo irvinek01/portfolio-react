45b9d9b0b42967be8df87534407ff99e8a5c4459
Arvin John Bautista <arvinyoung01@gmail.com>
2021-05-19 20:15:12 -0700

    Updated my Work, bootstraps, styles

--- src/components/pages/MyResume.js
+++ src/components/pages/MyResume.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 const MyResume = () => (
-  <div>
-    <h1>My Resume</h1>
-    <p>
-      You will be redirecting to google docs
-    </p>
+  <div className="pageContainer flex">
+    <div className="row">
+      <div className="col-l-4 col-md-4 col-sm-4">
+        <h2 className="pageHeading">My Resume</h2>
+      </div>
+      <div className="col-l-8 col-md-8 col-sm-8 content">
+        <p>
+          You will be redirected into Google docs
+        </p>
+      </div>
+    </div>
   </div>
 );
 
--- src/components/pages/MyWork.js
+++ src/components/pages/MyWork.js
@@ -1,16 +1,60 @@
 import React from "react";
 
 const MyWork = () => (
-  <div>
-    <h1>My Work</h1>
-    <p>
-      Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer
-      gravida dui mauris, ut interdum nunc egestas sed. Aenean sed mollis diam.
-      Nunc aliquet risus ac finibus porta. Nam quis arcu non lectus tincidunt
-      fermentum. Suspendisse aliquet orci porta quam semper imperdiet. Praesent
-      euismod mi justo, faucibus scelerisque risus cursus in. Sed rhoncus mollis
-      diam, sit amet facilisis lectus blandit at.
-    </p>
+  <div className="pageContainer flex">
+    <div className="row">
+      <div className="col-l-4 col-md-4 col-sm-4">
+        <h2 className="pageHeading">My Work</h2>
+      </div>
+      <div className="col-l-8 col-md-8 col-sm-8 content">
+        <div className="workContent">
+          <div className="row">
+            <div className="col-md-10 col-sm-10 big workBox">
+              <a href="https://irvinek01.github.io/world-of-words/">
+                <span>
+                  <h3>World of Words</h3>
+                  <p>A Word Defining Web App</p>
+                </span>
+              </a>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-md-5 col-sm-5 workBox">
+              <a href="https://neighborhood-appage.herokuapp.com/">
+                <span>
+                  <h3>Neighborhood Watch</h3>
+                  <p>A Rating Review Web App</p>
+                </span>
+              </a>
+            </div>
+            <div className="col-md-5 col-sm-5 workBox">
+              <a href="https://aj-tech-blog.herokuapp.com/">
+                <span>
+                  <h3>Tech Blog</h3>
+                  <p>A Blogsite</p>
+                </span>
+              </a>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-md-5 col-sm-5 workBox">
+              <span>
+                <h3>Figure 4</h3>
+                <p>Left</p>
+              </span>
+            </div>
+            <div className="col-md-5 col-sm-5 workBox">
+              <span>
+                <h3>Figure 5</h3>
+                <p>Right</p>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
